9a5d2090f9dea88bbad0d5f6169e6b452a4e2d29
Utkarsh-Subhedar <utkarshsubhedar2017@gmail.com>
2025-12-12 11:35:52 +0530

    project setup done and all dependencies installed

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="min-h-screen bg-red-500">ncsjdn</div>;
+}
+
+export default App;
